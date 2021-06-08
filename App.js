acd3b4f054f58f92e48c7b50e122d33a6b17941b
Jidaewook <psatdoctor@naver.com>
2021-06-08 19:27:04 +0900

    #Changing Initial View

--- App.js
+++ App.js
@@ -2,37 +2,12 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 import {View, Text} from 'react-native';
+import Home from './Screen/Home/Home';
 
 const App = () => {
 
-  // 상태선언
-  const [movie, setMovie] = useState([]);
-  // 함수선언
-  const getData = async() => {
-    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=262cfbaa555730595d97bf7a4c956d2a&language=en-US&page=1')
-      .then(res => {
-        console.log("11112451234123412341234123412341234", res.data.results)
-        setMovie(res.data.results);
-      })
-      .catch(err => {
-        console.log(err)
-      })
-  }
-
-  useEffect(() => {
-    getData()
-  }, [])
-
-
   return (
-    <View style={{alignItems: 'center', justifyContent: 'center'}}>
-      {movie.map(item => (
-        <Text>
-          {item.title}
-        </Text>
-        
-      ))}
-    </View>
+    <Home />
   );
 };
 
--- Screen/Home/Home.js
+++ Screen/Home/Home.js
@@ -4,11 +4,31 @@ import {Text, View, StyleSheet, } from 'react-native';
 import axios from 'axios';
 
 const Home = () => {
+
+    const [movies, setMovies] = useState([])
+
+    const getMovie = async() => 
+        axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=262cfbaa555730595d97bf7a4c956d2a&language=en-US&page=1')
+            .then(res => {
+                console.log(res)
+                setMovies(res.data.results)
+            })
+            .catch(err => {
+                console.log(err)
+            });
+
+    useEffect(() => {
+        getMovie()
+    }, [])
+    
+
     return (
         <View>
-            <Text>
-                Home
-            </Text>
+            {movies.map(item => (
+                <Text>
+                    {item.title}
+                </Text>
+            ))}
         </View>
     );
 };
